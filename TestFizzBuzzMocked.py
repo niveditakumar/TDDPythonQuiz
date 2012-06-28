b7a8e0ce3d065ac49996c8b07568711f834e93ae
niveditakumar <nivedita_kumar@persistent.co.in>
2012-06-28 10:09:54 +0530

    Update master

--- TestFizzBuzzMocked.py
+++ TestFizzBuzzMocked.py
@@ -3,7 +3,30 @@ import pymock
 import FizzBuzz
 """
 Q5. Write the psuedocode for the test_repport method, such that it uses PyMock
-    mock objects to test the report method of FizzBuzz. [5 pts]
+mock objects to test the report method of FizzBuzz. [5 pts]
+Ans:-
+
+def test_report(self):
+#mock the file handle
+mock_opener = self.mock()
+mock_file = self.mock()
+self.expectAndReturn(mock_opener.open('c:/temp/fizzbuzz_report.txt', 'w'), mock_file)
+mock_file.write("3 fizz \n")
+mock_file.close()
+
+#replay
+self.replay()
+
+#call API
+self.fb.report(numbers, opener=mock_opener.open)
+
+# verify
+self.verify()
+
+
+
+
+
 """
 class TestFizzBuzzMocked(pymock.PyMockTestCase):
         
@@ -17,7 +40,23 @@ class TestFizzBuzzMocked(pymock.PyMockTestCase):
         self.fb = None
 
     def test_report(self):
-        pass
+         #mock the file handle
+        mock_opener = self.mock()
+        mock_file = self.mock()
+        self.expectAndReturn(mock_opener.open('c:/temp/fizzbuzz_report.txt', 'w'), mock_file)
+        mock_file.write("3 fizz \n")
+        mock_file.close()
+
+        #replay
+        self.replay()
+
+        #call API
+        self.fb.report(numbers, opener=mock_opener.open)
+
+        # verify
+        self.verify()
+
+
 
 
 
